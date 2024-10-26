aac2fd793174d6a3ecad72fa0f6ba1f8c7f7a616
DivaniNL <dylanvannierop@gmail.com>
2024-10-26 20:19:44 +0200

    added more comments

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,12 @@ const popup = document.querySelector('.popup'); // Select your popup element
 
 // TAGLINE ACTUAL DATE
 
-const weekdays = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"];
-const months = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
-const today = new Date();
-const formattedDate = `${weekdays[today.getDay()]} ${today.getDate()} ${months[today.getMonth()]}`;
-document.getElementById("formattedDate").textContent = formattedDate;
+const weekdays = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"]; // Array van nederlandse weeknamen
+const months = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"]; // Arrays van nederlandse maandnamen
+const today = new Date(); // dagnummer
+const formattedDate = `${weekdays[today.getDay()]} ${today.getDate()} ${months[today.getMonth()]}`; // Het haalt de huidige dag van de week op via today.getDay() (0 = Zondag, 1 = Maandag, enz.). Hiermee wordt uit de array weekdays de juiste dagnaam gepakt in het nederlands. Het haalt de huidige dag van de maand op via today.getDate(). Het haalt de huidige maand op via today.getMonth() (0 = januari, 1 = februari, enz.). Hiermee wordt uit de array months de juiste maandnaam gepakt in het nederlands
+
+document.getElementById("formattedDate").textContent = formattedDate; // set text in div ecual to made current date in Dutch
 
 function showPopUp(popupname) {
     if (popupname) {
